babd0e9d7517bf0b43ae7c420f7d029842e60dff
Steven Walter <stevenrwalter@gmail.com>
2026-04-08 23:16:18 -0400

    armv8.rs: remove expect/unwrap
    Should return errors for the client to handle

--- src/armv8.rs
+++ src/armv8.rs
@@ -23,70 +23,76 @@ where
         }
     }
 
-    pub fn cpu_halt(&mut self)
+    pub fn cpu_halt(&mut self) -> Result<(), u8>
     {
         // Gate all
-        self.mem.write(self.cti_base + 0x140, 0).expect("write ctigate");
+        self.mem.write(self.cti_base + 0x140, 0)?;
 
         // Enable CTIOUTEN for channel 0
-        self.mem.write(self.cti_base + 0x0a0, 1).expect("write ctiouten");
+        self.mem.write(self.cti_base + 0x0a0, 1)?;
 
         // Generate HALT to core 0
-        self.mem.write(self.cti_base + 0x01c, 1).expect("write ctiouten");
+        self.mem.write(self.cti_base + 0x01c, 1)?;
 
         // ACK the halt
-        self.mem.write(self.cti_base + 0x010, 3).expect("write ctiouten");
+        self.mem.write(self.cti_base + 0x010, 3)?;
         // Wait for ACK
-        while self.mem.read(self.cti_base + 0x134).unwrap() != 0 {}
+        while self.mem.read(self.cti_base + 0x134)? != 0 {}
+        Ok(())
     }
 
-    pub fn cpu_resume(&mut self)
+    pub fn cpu_resume(&mut self) -> Result<(), u8>
     {
         // Gate all
-        self.mem.write(self.cti_base + 0x140, 0).expect("write ctigate");
+        self.mem.write(self.cti_base + 0x140, 0)?;
 
         // Enable CTIOUTEN for channel 1
-        self.mem.write(self.cti_base + 0x0a4, 2).expect("write ctiouten");
+        self.mem.write(self.cti_base + 0x0a4, 2)?;
 
         // Generate resume to core 0
-        self.mem.write(self.cti_base + 0x01c, 2).expect("write ctiouten");
+        self.mem.write(self.cti_base + 0x01c, 2)?;
 
         // ACK the resume
-        self.mem.write(self.cti_base + 0x010, 3).expect("write ctiouten");
+        self.mem.write(self.cti_base + 0x010, 3)?;
         // Wait for ACK
-        while self.mem.read(self.cti_base + 0x134).unwrap() != 0 {}
+        while self.mem.read(self.cti_base + 0x134)? != 0 {}
+        Ok(())
     }
 
-    fn assert_no_error(&mut self)
+    fn check_err(&mut self) -> Result<(), u8>
     {
-        let edscr = self.mem.read(self.cpu_base + 0x088).expect("read edscr");
+        let edscr = self.mem.read(self.cpu_base + 0x088)?;
         //println!("edscr {:x}", edscr);
         //println!("txu {:x}", edscr & (1 << 26));
-        assert_eq!(edscr & (1 << 6), 0);
+        if edscr & (1 << 6) != 0 {
+            Err(0)
+        } else {
+            Ok(())
+        }
     }
 
-    pub fn get_reg(&mut self, reg: u32) -> u64
+    pub fn get_reg(&mut self, reg: u32) -> Result<u64, u8>
     {
-        self.mem.write(self.cpu_base + 0x84, 0xd5130400 | reg).expect("write EDITR");
-        self.assert_no_error();
-        let orig_x0 = self.mem.read_block(self.cpu_base + 0x80, 8, false).expect("multi");
+        self.mem.write(self.cpu_base + 0x84, 0xd5130400 | reg)?;
+        self.check_err()?;
+        let orig_x0 = self.mem.read_block(self.cpu_base + 0x80, 8, false)?;
         let orig_x0_hi = orig_x0[0] as u64;
         let orig_x0_lo = orig_x0[3] as u64;
-        self.assert_no_error();
+        self.check_err()?;
 
-        orig_x0_hi << 32 | orig_x0_lo
+        Ok(orig_x0_hi << 32 | orig_x0_lo)
     }
 
-    pub fn set_reg(&mut self, reg: u32, val: u64)
+    pub fn set_reg(&mut self, reg: u32, val: u64) -> Result<(), u8>
     {
-        self.mem.write(self.cpu_base + 0x080, val as u32).expect("read edscr");
-        self.assert_no_error();
-        self.mem.write(self.cpu_base + 0x08c, (val >> 32) as u32).expect("read edscr");
+        self.mem.write(self.cpu_base + 0x080, val as u32)?;
+        self.check_err()?;
+        self.mem.write(self.cpu_base + 0x08c, (val >> 32) as u32)?;
         //// This seems to be needed on some A53 cores?
         self.mem.write(self.cpu_base + 0x090, 1 << 2).expect("write edrcr");
         // mrs x0, dbgdtr_el0
-        let _ = self.mem.write(self.cpu_base + 0x84, 0xd5330400 | reg).expect("write EDITR");
-        self.assert_no_error();
+        let _ = self.mem.write(self.cpu_base + 0x84, 0xd5330400 | reg)?;
+        self.check_err()
     }
 
     pub fn read_cpu(&mut self, offset: u32) -> Result<u32, u8>
